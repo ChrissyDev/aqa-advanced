755fa1fc743168d99cf09320e8d17e722f4fc9b7
ChrissyDev <stg.team01@gmail.com>
2024-02-05 18:58:08 +0200

    changed checkOrder values

--- js_functions_intro/task3.js
+++ js_functions_intro/task3.js
@@ -7,6 +7,6 @@ function checkOrder(available, ordered) {
         return "Your order is accepted.";
     }
 }
-console.log(checkOrder(20, 10), "Available amount is 21, and you ordered 7.");
-console.log(checkOrder(5, 10), "Available amount is 7, and you ordered 21.");
-console.log(checkOrder(15, 0), "Available amount is 21, and you ordered 0.");
+console.log(checkOrder(21, 7), "Available amount is 21, and you ordered 7.");
+console.log(checkOrder(7, 21), "Available amount is 7, and you ordered 21.");
+console.log(checkOrder(21, 0), "Available amount is 21, and you ordered 0.");
