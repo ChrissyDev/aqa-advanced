eb73bb809a70da5ab9d4d07d486f31007f57c6cb
ChrissyDev <stg.team01@gmail.com>
2024-02-19 17:58:22 +0200

    homework eight on proper branch

---
+++ objects/task1.js
@@ -0,0 +1,7 @@
+const student = {};
+student.firstName = "James";
+student.lastName = "Clear";
+student.age = 38;
+student.courses = ["Математика", "Історія", "Програмування"];
+
+console.log(student);
---
+++ objects/task2.js
@@ -0,0 +1,9 @@
+const book = {
+    title: "Atomic Habits",
+    author: "James Clear",
+    year: 2018
+};
+
+const { title, author } = book;
+console.log("Title:", title);
+console.log("Author:", author);
